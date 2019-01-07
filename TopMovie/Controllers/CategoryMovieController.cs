4a1e4821e90be715d4b961654b9aeb1fd6797573
Huy <anhhuydng@gmail.com>
2019-01-07 18:02:01 +0700

    Fix DataTables Finished Category Movie

--- TopMovie/Controllers/CategoryMovieController.cs
+++ TopMovie/Controllers/CategoryMovieController.cs
@@ -47,11 +47,45 @@ namespace TopMovie.Controllers
         }
 
         [HttpPost]
-        public ActionResult CreateCategoryMovie([FromBody] TbCategoryMovie categoryData)
+        public List<TbCategoryMovie> GetAll()
+        {
+            CategoriesMoviesModel model = new CategoriesMoviesModel();
+            model.categories = context.TbCategoryMovie.ToList();
+            model.movies = context.TbMovie.ToList();
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new { data = model.categories.ToList() });
+            return model.categories.ToList();
+        }
+
+        [HttpGet]
+        public TbCategoryMovie GetById(int id)
+        {
+            return context.TbCategoryMovie.FirstOrDefault(c=>c.Id == id);
+        }
+
+        [HttpPost]
+        public int Create([FromBody] TbCategoryMovie categoryData)
         {
             context.TbCategoryMovie.Add(categoryData);
-            context.SaveChanges();
-            return View("Index", "CategoryMovie");
+            var result = context.SaveChanges();
+            return result;
+        }
+
+        [HttpPost]
+        public int Update([FromBody] TbCategoryMovie categoryData)
+        {
+            var oldCategory = context.TbCategoryMovie.FirstOrDefault(c=>c.Id == categoryData.Id);
+            context.Entry<TbCategoryMovie>(oldCategory).CurrentValues.SetValues(categoryData);
+            var result = context.SaveChanges();
+            return result;
+        }
+
+        [HttpGet]
+        public int Delete(int id)
+        {
+            var category = context.TbCategoryMovie.FirstOrDefault(c => c.Id == id);
+            context.TbCategoryMovie.Remove(category);
+            var result = context.SaveChanges();
+            return result;
         }
     }
 }
