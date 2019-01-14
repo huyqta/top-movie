3c69df2979529afd66faa59555255751bb5c0045
huyqta <anhhuydng@gmail.com>
2019-01-14 21:45:04 +0700

    Remove Google SDK

--- TopMovie/Program.cs
+++ TopMovie/Program.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Google.Apis.Drive.v3;
+﻿using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace TopMovie
 {
     public class Program
     {
-        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        //static string[] Scopes = { DriveService.Scope.DriveReadonly };
         static string ApplicationName = "Drive API .NET Quickstart";
 
         public static void Main(string[] args)
--- TopMovie/Startup.cs
+++ TopMovie/Startup.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Movie.Services.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace TopMovie
 {
